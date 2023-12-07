19b2632d6c69ed4c4e43304fe3e4440f0e8a5e1e
teplovslava <slava.teplov.2014@mail.ru>
2023-12-07 14:43:28 +0300

    add hints

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const node = document.querySelector('#root')
 const progressBar = new ProgressBar(node,'Progress')
 
 
+// progressBar.changeAnimationOn() to turn on animation
+// progressBar.changeAnimationOff() to turn off animation
+
+// progressBar.changeHideOn() to turn off visibility
+// progressBar.changeHideOff() to turn on visibility
+
+// progressBar.changeValue(val) to change value (val : string | number)
+
 
